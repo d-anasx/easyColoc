38b8e3e362488995ce15ef18587b5d75ee702877
d-anasx <djamaouianas@gmail.com>
2026-03-01 17:49:55 +0000

    Remove profile management routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -40,10 +40,4 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin'])->group(fun
     Route::get('stats', [AdminController::class, 'stats'])->name('stats');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__ . '/auth.php';
